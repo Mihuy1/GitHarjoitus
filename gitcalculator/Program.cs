38bfdc84d465e586dfff171f8f74d2bf0c438552
Mihuy1 <s2000954@edu.bc.fi>
2022-02-03 10:37:23 +0200

    lisätty kaikki takaisin

---
+++ gitcalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace gitcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ensimmäinen numero: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Toinen numero: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = input + input1;
+
+            Console.WriteLine("Vastaus: " + sum);
+        }
+    }
+}
